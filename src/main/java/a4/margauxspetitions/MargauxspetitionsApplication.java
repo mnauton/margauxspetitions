11693a7a6ab4c174755d53ba8eeadacbb5fb7072
Margaux Nauton-Fourteu <mnauton@gmail.com>
2024-11-12 10:14:20 +0100

    Initial commit with code generated by IntelliJ

---
+++ src/main/java/a4/margauxspetitions/MargauxspetitionsApplication.java
@@ -0,0 +1,13 @@
+package a4.margauxspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargauxspetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MargauxspetitionsApplication.class, args);
+    }
+
+}
