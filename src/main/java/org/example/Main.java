170c58e59162b0b941b5f8aad463befd07e96dd6
Margaux Nauton-Fourteu <mnauton@gmail.com>
2024-11-21 11:49:33 +0100

    Adding main file

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.example"})
+
+public class Main extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
